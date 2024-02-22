89fea576f285cd4e16dcdd5ddd50fca3b33c1fd4
Faz-code <154399836+Faz-code@users.noreply.github.com>
2024-02-22 18:40:22 +0100

    Salems studios

---
+++ Salems Portfolio/Pages/Home Page/Main/home.js.js
@@ -0,0 +1,10 @@
+// Check internet connection
+if (navigator.onLine) {
+    // Proceed with loading the app content
+    // Show your loading screen here
+    // ...
+  } else {
+    // Display an error message or prompt to connect to the internet
+    alert("Please check your internet connection and try again.");
+  }
+  
